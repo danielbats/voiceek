78418ac643d90788f630fabac8394e74eebc3f0a
danielbats <daniel_sbs12@hotmail.com>
2018-03-26 03:05:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+"""Generate .exe
+You will need this file to build the .exe
+Please, use "pip install py2exe" in the command prompt to get the requeriments properly set.
+"""
+
 from distutils.core import setup
 import py2exe
 
